78b7e648b1809a148a7428f74908db7d67fb89d5
FenzDev <68622361+FenzDev@users.noreply.github.com>
2025-05-23 02:28:49 +0100

    Created BldTools and DbgTools initially.

---
+++ Source/Fenzwork.BuildingTools/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Fenzwork.BuildingTools
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO : BuildingTools
+        }
+    }
+}
---
+++ Source/Fenzwork.DebuggingTools/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Fenzwork.DebuggingTools
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO : Debugging Tools
+        }
+    }
+}
